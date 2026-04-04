b3ed5a153d6272f679605a8a74339424c67176e2
Ward Weistra <ward@fire.ly>
2026-04-05 00:29:28 +0200

    Make English the fallback language

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -98,8 +98,8 @@ abstract class AppLocalizations {
 
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
-    Locale('de'),
     Locale('en'),
+    Locale('de'),
     Locale('es'),
     Locale('fr'),
     Locale('nl'),
