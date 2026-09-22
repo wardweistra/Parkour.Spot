5cdbd0040c8c3a59405d9d4068f103fbc02f9f49
Ward Weistra <ward@fire.ly>
2026-09-22 14:52:55 +0200

    Automatic fixes

--- lib/widgets/country_flag_badge.dart
+++ lib/widgets/country_flag_badge.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cached_network_image_platform_interface/cached_network_image_platform_interface.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 /// Compact country flag for cards and location rows.
